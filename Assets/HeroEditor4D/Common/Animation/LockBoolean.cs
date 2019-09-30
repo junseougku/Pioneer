dba79765e11c1052d907a49c4991557488b2783c
newjsg <37951992+junseougku@users.noreply.github.com>
2019-09-30 17:28:28 +0900

    캐릭터 마우스클릭
    마우스 클릭에의해 움직이고
    간단한 애니메이션 추가

---
+++ Assets/HeroEditor4D/Common/Animation/LockBoolean.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.HeroEditor4D.Common.Animation
+{
+	public class LockBoolean : StateMachineBehaviour
+	{
+		public string ParameterName;
+		public float ExitTime;
+
+		public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+			animator.SetBool(ParameterName, true);
+		}
+
+		public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+			if (stateInfo.normalizedTime >= ExitTime)
+			{
+				animator.SetBool(ParameterName, false);
+			}
+		}
+	}
+}
---
+++ Assets/Leader/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector3 targetPosition;
+    public Animator animator;
+    enum STATE { IDLE, WALK}
+    STATE eState = STATE.IDLE;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //animator = GetComponent<Animator>();
+        targetPosition = Vector3.zero;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))   //마우스 좌측 버튼을 누름.
+        {
+            eState = STATE.WALK;
+            animator.SetTrigger("Walk");
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (targetPosition.x < transform.position.x) transform.localScale = new Vector3(-1,1,1);
+            else transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(eState == STATE.WALK)
+        {
+            
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, 1.0f * Time.deltaTime);       //클릭한 지점으로 이동
+            float dis = Vector2.Distance(transform.position, targetPosition);
+            if (dis <= 0.01f)               //클릭한 지점에 다왔다면
+            {
+                eState = STATE.IDLE;
+                animator.SetTrigger("Idle");
+                targetPosition = Vector3.zero;
+            }
+        }
+
+    }
+}
